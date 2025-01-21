b2235f93dbe592ab14004add5c43d800cdb036ed
anhtu1707 <anhtu17602001@gmail.com>
2025-01-21 09:07:51 +0700

    Update routing configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <Router basename="/phucanhome">
+    <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
