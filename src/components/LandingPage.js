80194c9a43af1d30b654e20f613ee5c3c1b07b92
anhtu1707 <142393219+anhtu1707@users.noreply.github.com>
2025-01-22 11:07:45 +0700

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -34,7 +34,7 @@ const LandingPage = () => {
         preload="auto"
         className="background-video"
       >
-        <source src="https://drive.google.com/file/d/1_9UbSdhpleFwBEVrZ4new6Knk9VhlMbT/review"/>
+        <source src="www.dailymotion.com/embed/video/k2IpnFGMwiAITvChy54"/>
         Trình duyệt của bạn không hỗ trợ video tag.
       </video>
       
